d98568df3daa24c2e32f2940d4624a37ab1bc22a
yangduoZOU <zyd1013482789@gmail.com>
2022-11-17 00:34:46 +0100

    yangduo

---
+++ tp01/MaPremierConsole.java
@@ -0,0 +1,15 @@
+package tp01;
+
+import util.Console;
+
+public class MaPremierConsole {
+
+	public static void main(String[] args) {
+		Console c = new Console();
+		c.println("Yangduo");
+		c.println("ZOU");
+		
+
+	}
+
+}
---
+++ tp01/SimpleProgramme1.java
@@ -0,0 +1,9 @@
+package tp01;
+
+public class SimpleProgramme1 {
+	public static void main(String[]args) {
+		System.out.println("ZOU");
+	}
+	
+
+}
