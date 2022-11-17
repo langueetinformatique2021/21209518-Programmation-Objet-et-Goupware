df9c2d1fd204b377200f1863df453ff96f55c89a
yangduoZOU <zyd1013482789@gmail.com>
2022-11-17 11:00:08 +0100

    17/11

---
+++ tp07/Exe4.java
@@ -0,0 +1,35 @@
+package tp07;
+
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+/** dérivée de Calendar implémentant un nouvel attribut cours 
+ * et trois nouvelles méthodes creation() afficher() et duree() */
+public class Exe4 extends GregorianCalendar {
+	
+	private boolean cours;
+	
+	public Exe4() {
+		Date date = new Date();
+		this.setTime(date);	
+	}
+	
+	/** affiche la date et l’heure de la création de l’objet */
+	public void creation() {
+		System.out.println("TIME: " + this.getTime());
+	}
+	
+	/** affiche la date et l’heure courante */
+	public void afficher() {
+		Date date = new Date();
+		GregorianCalendar calendar = new GregorianCalendar();
+		calendar.setTime(date);	
+		System.out.println("TIME: " + calendar.getTime());
+	}
+	
+	/** affiche le nombre de secondes depuis la création de l’objet */
+	public void duree() {}
+	
+	
+
+}
---
+++ tp07/TestExe4.java
@@ -0,0 +1,17 @@
+package tp07;
+
+import util.Keyboard;
+
+public class TestExe4 {
+
+	public static void main(String[] args) {
+		
+		Exe4 c = new Exe4();
+		c.creation();
+		Keyboard.getInt("attente");
+		c.creation();
+		c.afficher();
+
+	}
+
+}
