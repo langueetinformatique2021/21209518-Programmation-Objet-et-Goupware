09c094194bb86853537fe5e9f2b892be9836dc11
yangduoZOU <zyd1013482789@gmail.com>
2022-12-01 10:16:32 +0100

    exe1

--- tp09/MonPremierFichierTexte.java
+++ tp09/MonPremierFichierTexte.java
@@ -1,5 +1,21 @@
 package tp09;
 
+/**
+ * écrit dans le fichier texte « Nom.txt » les noms des membres du groupe
+ * @author yangduo
+ */
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class MonPremierFichierTexte {
 
+	public static void fileWriterMethod(String filepath, String content) throws IOException {
+		try (FileWriter fileWriter = new FileWriter(filepath)) {
+			fileWriter.append(content);
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		fileWriterMethod("/Users/yangduo/git/21209518-Programmation-Objet-et-Goupware/tp09/Nom.txt", "test");
+	}
 }
